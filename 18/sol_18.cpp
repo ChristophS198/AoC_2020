10e8cb66e0d7e00125df5edcc9e405859552795f
Sommerauer, Christoph <christoph.cs.sommerauer@gmail.com>
2023-10-14 15:21:52 +0200

    Add solution for day 18 part 1

---
+++ 18/sol_18.cpp
@@ -0,0 +1,136 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <sstream>
+#include <fstream>
+
+#include "../utility.h"
+
+using TMath = int64_t;
+
+std::pair<TMath,size_t> convert_to_num(const std::string c_vec, size_t pos);
+size_t skip_space(const std::string &expr, size_t pos);
+TMath eval_operator(char op, TMath op1, TMath op2);
+std::pair<TMath,size_t> eval_expr(const std::string &expr, size_t pos, TMath intermediate_sum);
+
+
+TMath sol_18_1(const std::string &file_path)
+{
+    std::vector<std::string> math_expressions = read_string_vec_from_file(file_path);
+    TMath sum{ 0 };
+
+    for (auto &expr : math_expressions)
+    {
+        std::pair<TMath,size_t> res = eval_expr(expr, 0,0);
+        sum += res.first;
+    }
+
+    return sum;
+}
+
+
+TMath sol_18_2(const std::string &file_path)
+{
+
+
+    return 0;
+}
+
+/*
+Evaluate string from left to right
+*/
+
+std::pair<TMath,size_t> eval_expr(const std::string &expr, size_t pos, TMath intermediate_sum)
+{
+    char op{ '+' };
+
+    pos = skip_space(expr,pos); // move pos to next char != space 
+    std::pair<TMath,size_t> tmp_dig{0,0};
+
+    while (pos < expr.length())
+    {
+        char c_1{ expr.at(pos) };
+
+        if (std::isdigit(static_cast<unsigned char>(c_1)))
+        {
+            tmp_dig = convert_to_num(expr,pos);
+            pos = tmp_dig.second;
+            intermediate_sum = eval_operator(op, intermediate_sum, tmp_dig.first);
+        }
+        else
+        {
+            switch (c_1)
+            {
+            case '+':
+                // update operator 
+                op = expr.at(pos);
+                ++pos;
+                break;
+            case '*':
+                // update operator 
+                op = expr.at(pos);
+                ++pos;
+                break;
+            case '(':
+                ++pos;
+                tmp_dig = eval_expr(expr,pos,0);
+                pos = tmp_dig.second;
+                intermediate_sum = eval_operator(op, intermediate_sum, tmp_dig.first);
+                break;
+            case ')':
+                ++pos;
+                return { intermediate_sum,pos };
+                break;
+            default:
+                std::ostringstream err_msg; 
+                err_msg << "eval_expr found an invalid character " << c_1 << " at pos" << pos << "!";
+                throw std::invalid_argument(err_msg.str());
+                break;
+            }
+
+        }
+        pos = skip_space(expr,pos); // move pos to next char != space 
+    }
+    
+    return { intermediate_sum,pos };
+}
+
+TMath eval_operator(char op, TMath op1, TMath op2)
+{
+    switch (op)
+    {
+    case '+':
+        return op1 + op2;
+        break;
+    case '*':
+        return op1 * op2;
+        break;
+    
+    default:
+        std::ostringstream err_msg; 
+        err_msg << "eval_operator called with invalid operator" << op << "!";
+        throw std::invalid_argument(err_msg.str());
+        return -1;
+        break;
+    }
+}
+
+size_t skip_space(const std::string &expr, size_t pos)
+{
+    while (pos < expr.length() && expr.at(pos)==' ')
+    {
+        ++pos;
+    }
+    return pos;
+}
+
+std::pair<TMath,size_t> convert_to_num(const std::string c_vec, size_t pos)
+{
+    TMath num{};
+    while (pos < c_vec.length() && std::isdigit(static_cast<unsigned char>(c_vec.at(pos))))
+    {
+        num = num*10 + c_vec.at(pos) - '0';
+        ++pos;
+    }
+    return {num, pos};
+}
--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include "15/sol_15.cpp"
 #include "16/sol_16.cpp"
 #include "17/sol_17.cpp"
+#include "18/sol_18.cpp"
 #include <filesystem>
 
 std::string get_input_file_name(int day)
@@ -58,9 +59,11 @@ int main(int /*argc*/, char** /*argv*/)
     std::cout << sol_15_2(get_input_file_name(15)) << std::endl;
     std::cout << sol_16_1(get_input_file_name(16)) << std::endl;
     std::cout << sol_16_2(get_input_file_name(16)) << std::endl;
-*/
     std::cout << sol_17_1(get_input_file_name(17)) << std::endl;
     std::cout << sol_17_2(get_input_file_name(17)) << std::endl;
+*/
+    std::cout << sol_18_1(get_input_file_name(18)) << std::endl;
+    std::cout << sol_18_2(get_input_file_name(18)) << std::endl;
 
     return 0;
 }
