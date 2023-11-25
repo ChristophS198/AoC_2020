7490f747dd2848f4ef9e970d592713307dad0fc4
Sommerauer, Christoph <christoph.cs.sommerauer@gmail.com>
2023-11-25 19:52:50 +0100

    Add description for day 25

---
+++ 25/sol_25.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <set>
+
+#include "../utility.h"
+
+
+int sol_25_1(const std::string &file_path)
+{
+
+
+    return 0;
+}
+
+int sol_25_2(const std::string &file_path)
+{
+
+    
+    return 0;
+}
